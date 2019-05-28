3451a669d91565384ce6b7377a0fb16de4188f0e
yamakentoc <yamakentoc@gmail.com>
2019-05-29 02:22:42 +0900

    250件を1m~2m以内の間にランダムに表示

--- Assets/Scripts/Circlecontroller.cs
+++ Assets/Scripts/Circlecontroller.cs
@@ -7,13 +7,13 @@ public class Circlecontroller : MonoBehaviour {
 
     [Header("分布")]
     [SerializeField] Transform CenterPosition;                 // 対象オブジェクト
-    private int ArrangementMaxRedius = 1000;         // 配置位置の最大半径
-    private int ArrangementMinRedius = 500;         // 配置位置の最小半径
-    private int ArrangementHeight = 4;              // 配置位置の高さ
+    private float ArrangementMaxRedius = 2.0f;         // 配置位置の最大半径
+    private float ArrangementMinRedius = 1.0f;         // 配置位置の最小半径
+    private float ArrangementHeight = 4.0f;              // 配置位置の高さ
 
     [Header("個数")]
     [SerializeField] GameObject CreaturePrefab;                 // 対象オブジェクト
-    [SerializeField] int CreatureLength = 100;                 // 配置位置の最大
+    private int CreatureLength = 250;                 // 配置位置の最大
 
     void Start() {
         GameObject[] CreatureRange = new GameObject[CreatureLength];
@@ -21,20 +21,19 @@ public class Circlecontroller : MonoBehaviour {
         double minR = Math.Pow(ArrangementMinRedius, 2);
         for (int i = 0; i < CreatureRange.Length; i++) {
             while (CreatureRange[i] == null) {
-                int x = UnityEngine.Random.Range(-ArrangementMaxRedius, ArrangementMaxRedius);
-                int z = UnityEngine.Random.Range(-ArrangementMaxRedius, ArrangementMaxRedius);
-
+                float x = UnityEngine.Random.Range(-ArrangementMaxRedius, ArrangementMaxRedius);
+                float z = UnityEngine.Random.Range(-ArrangementMaxRedius, ArrangementMaxRedius);
                 double xAbs = Math.Abs(Math.Pow(x, 2));
                 double zAbs = Math.Abs(Math.Pow(z, 2));
 
-                // 特定の範囲内化確認
+                // 特定の範囲内か確認
                 if (maxR > xAbs + zAbs && xAbs + zAbs > minR) {
-                    int y = UnityEngine.Random.Range(-2, ArrangementHeight);
+                    float y = UnityEngine.Random.Range(-2, ArrangementHeight);
                     GameObject go = Instantiate(
                         CreaturePrefab, // 個体のオブジェクト
                         (new Vector3(x, y, z)) + CenterPosition.position, // 初期座標
-                        Quaternion.identity // 回転位置
-                    );
+                        Quaternion.identity); // 回転位置
+                    go.transform.LookAt(this.CenterPosition.position);
                     CreatureRange[i] = go;
                 }
             }
