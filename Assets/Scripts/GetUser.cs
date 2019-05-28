f7be12a2e8380de9ea5cd666882d78dd404e3e3f
yamakentoc <yamakentoc@gmail.com>
2019-05-28 15:06:59 +0900

    userのデータをget

---
+++ Assets/Scripts/GetUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetUser : MonoBehaviour {
+
+    private string url = "http://localhost:8080";
+
+    void Start() {
+        StartCoroutine(GetUserData());
+    }
+    
+    void Update() {
+        
+    }
+
+    private IEnumerator GetUserData() {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+        if (request.isHttpError || request.isNetworkError) {
+            Debug.Log("エラー: " + request.error);
+        } else {
+            Debug.Log("ステータスko-do: " + request.responseCode);
+            var identifiedUsers = JsonUtility.FromJson<IdentifiedUsers>(request.downloadHandler.text);
+            foreach(User user in identifiedUsers.users) {
+                Debug.Log("userName: " + user.name + ", Relevance: " + user.relevance);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class IdentifiedUsers {
+    public int count;
+    public List<User> users;
+}
+
+[Serializable]
+public class User {
+    public string name;
+    public long id;
+    public string profileImageURL;
+    public float relevance;
+}
