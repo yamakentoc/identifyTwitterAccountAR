ed7216edfdefe6d1ac928d91de63944a9f69b8e6
yamakentoc <yamakentoc@gmail.com>
2019-05-31 12:34:31 +0900

    ユーザーのページを開けるように

--- Assets/Scripts/Circlecontroller.cs
+++ Assets/Scripts/Circlecontroller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Circlecontroller : MonoBehaviour {
 
--- Assets/Scripts/TapUserIcon.cs
+++ Assets/Scripts/TapUserIcon.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.iOS;
+using UnityEngine.EventSystems;
 
 public class TapUserIcon : MonoBehaviour {
-    
+
+    private User showingUser;
+
     void Update() {
         TouchDown();
     }
@@ -12,6 +15,9 @@ public class TapUserIcon : MonoBehaviour {
     private void TouchDown() {
         //if (0 < Input.touchCount) {
         //    Touch t = Input.GetTouch(0);
+        //    if (EventSystem.current.IsPointerOverGameObject(t.fingerId)) {
+        //        return;
+        //    }
         //    if (t.phase == TouchPhase.Began) {
         //        Ray ray = Camera.main.ScreenPointToRay(t.position);
         //        RaycastHit hit = new RaycastHit();
@@ -24,15 +30,23 @@ public class TapUserIcon : MonoBehaviour {
         //}
 
         if (Input.GetMouseButtonDown(0)) {
+            if (EventSystem.current.IsPointerOverGameObject()) {
+                return;
+            }
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit)) {
                 Debug.Log("ひや");
                 hit.transform.gameObject.GetComponent<UserIcon>().Hit();
+                showingUser = hit.transform.gameObject.GetComponent<UserIcon>().user;
             }
         }
+    }
 
+    public void TapButton() {
+        string url = "https://twitter.com/" + showingUser.screen_name;
+        Application.OpenURL(url);
     }
 
 }
